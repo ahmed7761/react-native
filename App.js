a206e0b77f1ef3279944a4e5193c5ec4c922f43a
ahmed7761 <76873374+ahmed7761@users.noreply.github.com>
2021-09-26 19:19:49 +0600

    initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+//import React, {Component} from 'react';
+//import {View, Text} from 'react-native';
+//
+//class App extends Component {
+//    render() {
+//        return (
+//            <View>
+//                <Text>Hello World</Text>
+//            </View>
+//        );
+//    }
+//}
+//
+//export default App;
+
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+    return (
+            <View>
+                <Text>Hello World</Text>
+            </View>
+           );
+}
+
+export default App;
+
