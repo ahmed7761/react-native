afdb33799bcdc0947fff5f3e403c739d90ce2b2a
ahmed7761 <76873374+ahmed7761@users.noreply.github.com>
2021-09-27 16:48:17 +0600

    update state on button press

--- Components/ClassState.js
+++ Components/ClassState.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ClassState extends Component {
 
@@ -18,12 +19,21 @@ class ClassState extends Component {
 //            city: 'Dhaka',
 //    }
 
+     change = () => {
+        this.setState({
+            name: "Ahmed",
+            age: "10",
+            city: "CTG",
+        })
+    }
+
     render() {
         return (
             <View>
                 <Text>{ this.state.name }</Text>
                 <Text>{ this.state.age }</Text>
                 <Text>{ this.state.city }</Text>
+                <Button title="Change Value" onPress={this.change} />
             </View>
         );
     }
